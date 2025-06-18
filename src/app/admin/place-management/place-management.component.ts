a67476efd80403f229aaa3d145c5cfa06c7e1a80
thecrackmandieng <moustaphadieng0405@gmail.com>
2025-06-18 15:55:45 +0000

    comm

--- src/app/admin/place-management/place-management.component.ts
+++ src/app/admin/place-management/place-management.component.ts
@@ -1,28 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from '../../header/header.component';
+import { RouterModule } from '@angular/router';
+import { SidebarComponent } from "../../sidebar/sidebar.component";
 
 @Component({
   selector: 'app-place-management',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    HeaderComponent,
+    SidebarComponent
+  ],
   templateUrl: './place-management.component.html',
   styleUrls: ['./place-management.component.css']
 })
 export class PlaceManagementComponent implements OnInit {
-  places: any[] = []; // Remplacez "any" par une interface ou un type approprié
+  places: any[] = [];
+  selectedParkingId: number | null = null;
+  uniqueParkingIds: number[] = [];
+  licensePlates: { [key: number]: string } = {
+    1: 'ABC123',
+    4: 'XYZ789',
+    6: 'DEF456'
+  };
 
   ngOnInit(): void {
-    // Logique pour charger les places
     this.places = [
-      { id: 1, name: 'Place 1', parkingId: 1 },
-      { id: 2, name: 'Place 2', parkingId: 1 }
+      { id: 1, name: 'Place 1', parkingId: 1, isOccupied: true },
+      { id: 2, name: 'Place 2', parkingId: 1, isOccupied: false },
+      { id: 3, name: 'Place 3', parkingId: 1, isOccupied: true },
+      { id: 4, name: 'Place 4', parkingId: 1, isOccupied: true },
+      { id: 5, name: 'Place 5', parkingId: 3, isOccupied: false },
+      { id: 6, name: 'Place 6', parkingId: 3, isOccupied: true },
     ];
+    this.uniqueParkingIds = [...new Set(this.places.map(place => place.parkingId))];
+    // Initialiser selectedParkingId avec le premier parking ID
+    this.selectedParkingId = this.uniqueParkingIds.length > 0 ? this.uniqueParkingIds[0] : null;
   }
 
   addPlace(name: string, parkingId: number): void {
-    // Logique pour ajouter une place
-    this.places.push({ id: this.places.length + 1, name, parkingId });
+    this.places.push({ id: this.places.length + 1, name, parkingId, isOccupied: false });
+    this.uniqueParkingIds = [...new Set(this.places.map(place => place.parkingId))];
   }
 
-  deletePlace(id: number): void {
-    // Logique pour supprimer une place
-    this.places = this.places.filter(place => place.id !== id);
+  onParkingSelect(): void {
+    // Cette méthode est appelée lorsque la sélection change, mais la logique de filtrage est déjà gérée par getFilteredPlaces
+  }
+
+  getFilteredPlaces(): any[] {
+    if (this.selectedParkingId === null) {
+      return this.places;
+    } else {
+      return this.places.filter(place => place.parkingId === this.selectedParkingId);
+    }
   }
 }
